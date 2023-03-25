94b693679f2087785d50dca9add1250c4d8830ea
lankabelgezogen <cybertom1234@gmail.com>
2023-03-25 19:10:02 +0100

    basic JS structure, functionality missing

--- website/app.js
+++ website/app.js
@@ -1,5 +1,29 @@
-/* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const api_key = 'e6d6fbc638daf47c785e60f9dd1dcad1';
+const baseUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=';
+// Event listener to add function to existing HTML DOM element
+document.getElementById('generate').addEventListener('click', generateAPICall)
+/* Function called by event listener */
+function generateAPICall(e){
+    const zip = document.getElementById('zip').value;
 
-// Create a new date instance dynamically with JS
-let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+    getDataFromAPI(baseUrl, zip, api_key);
+}
+/* Function to GET Web API Data*/
+async function getDataFromAPI(url = '', zip = '', key = ''){
+    const response = await fetch(url + zip + key);
+    try {
+        const weatherData = response.json();
+    } catch(error) {
+        console.log(error);
+    }
+}
+/* Function to POST data */
+async function postData (path = '', data = {}) {
+
+}
+
+/* Function to GET Project Data */
+async function updateUI(){
+    
+}
